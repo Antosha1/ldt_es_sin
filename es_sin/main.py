92f0a39dc6439dfc613f7dd3bcf8371be36fdd45
yakalex <yakushev.ayu@yandex.ru>
2020-10-30 22:44:27 +0300

    Created python package

---
+++ es_sin/main.py
@@ -0,0 +1,7 @@
+def main():
+    print('started')
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='es_sin',
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Swimming in network",
+    description="",
+
+    install_requires=[
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'start_es_sin = es_sin.main:main'
+        ]
+    })
