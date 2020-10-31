a17ff31a0b7903e0504bfb356ccab0c35cb45e33
yakalex <yakushev.ayu@yandex.ru>
2020-10-31 10:13:24 +0300

    Added simple web interface

--- es_sin/es_compute.py
+++ es_sin/es_compute.py
@@ -8,6 +8,7 @@ def get_product(location):
 
 
 def get_location(product):
+    return 'result'
     valuable_features = get_valuable_features(product)
     possible_locations = get_possible_locations(product)
     best_location = get_best_location(possible_locations, valuable_features)
--- es_sin/main.py
+++ es_sin/main.py
@@ -1,9 +1,28 @@
 from es_sin.es_compute import get_location
+from flask import Flask, render_template, redirect, url_for, request
+
+
+app = Flask(__name__)
+results = []
+
+
+@app.route('/', methods=['GET'])
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', results=results)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    product = request.form['product']
+    global results
+    results = []
+    results.append(get_location(product))
+    return redirect(url_for('main'))
 
 
 def main():
-    product = input()
-    print(get_location(product))
+    app.run()
     pass
 
 
