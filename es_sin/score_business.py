5a7ee644afe3350e986b7b6dec69dd8a46b211f9
Antosha1 <sotnikov.ad@phystech.edu>
2020-10-31 16:11:06 +0300

    antoxa

--- es_sin/score_business.py
+++ es_sin/score_business.py
@@ -1,3 +1,29 @@
-def score_business(business):
-    score = 0
-    return score
+import math
+
+
+def softmax(profits):
+    sum_ = sum(math.exp(profits))
+    res = []
+
+    for profit in profits:
+        x = (math.exp(profit)) / sum_
+        res.append(x)
+
+    return res
+
+
+def get_score(business_names):
+    d = {}
+    for business_name in business_names:
+        # select 'Общая итоговая сумма в чеках' as profit
+        # from kkt_table
+        # where 'Наименование' == business_name
+        d[business_name] = profit
+
+    profits = [profit for profit in d.values()]
+    scores = softmax(profits)
+    d_scores = {profits[i]: scores[i] for i in range(len(profits))}
+
+    business_scores = {business_name: d_scores[profit] for business_name, profit in d.items()}
+
+    return business_scores    
